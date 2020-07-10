809fcc3ba574f2d0b3be4855e778d11fef4fbb09
kanwaljeetsm <thekanwaljeet@gmail.com>
2020-07-10 20:02:48 +0530

    added condition

--- TossACoin.java
+++ TossACoin.java
@@ -6,9 +6,7 @@ import java.util.Scanner;
 public class TossACoin {
 	
 	static {
-		
 		System.out.println("Let's toss a Coin!");
-		
 	}
 
 	public static void main(String[] args) {
@@ -17,34 +15,30 @@ public class TossACoin {
 		int result, randomNum;
 		
 		while(true) {
-		
-			System.out.println("\nPress '1' to Toss    and    Press '2' to Exit");
+			System.out.println("\nPress '1' to Toss    and    Press '0' to Exit");
 			Scanner scanner = new Scanner(System.in);
+			Random random = new Random();
 			result = scanner.nextInt();
 		
 				if(result == 1) {
-		
-					Random random = new Random();
 					randomNum = random.nextInt(2);
 					
 					if(randomNum == 0) {
-						
 						System.out.println("Heads!");
-						
 					}
 					
 					else {
-						
 						System.out.println("Tails!");
-						
 					}
 				}
 		
 				else if(result == 0) {
-		
 					System.exit(0);
-		
+				}
+				
+				else {
+					System.out.println("Invalid Input!");
 				}
 			}
 		}
-}
+	}
