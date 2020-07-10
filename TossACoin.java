7ac058ec8e67f2fd892ce141a257360bf80f6e33
kanwaljeetsm <thekanwaljeet@gmail.com>
2020-07-10 19:02:26 +0530

    Complete Program

---
+++ TossACoin.java
@@ -0,0 +1,50 @@
+package kanwaljeetsm;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class TossACoin {
+	
+	static {
+		
+		System.out.println("Let's toss a Coin!");
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int result, randomNum;
+		
+		while(true) {
+		
+			System.out.println("\nPress '1' to Toss    and    Press '2' to Exit");
+			Scanner scanner = new Scanner(System.in);
+			result = scanner.nextInt();
+		
+				if(result == 1) {
+		
+					Random random = new Random();
+					randomNum = random.nextInt(2);
+					
+					if(randomNum == 0) {
+						
+						System.out.println("Heads!");
+						
+					}
+					
+					else {
+						
+						System.out.println("Tails!");
+						
+					}
+				}
+		
+				else if(result == 0) {
+		
+					System.exit(0);
+		
+				}
+			}
+		}
+}
